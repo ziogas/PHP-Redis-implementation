973314e69bd3f922b72499c01564270f238f2663
Arminas Žukauskas <ziogasas@gmail.com>
2016-12-28 19:40:35 +0200

    Making code psr2 compatible, code style changes

--- redis.php
+++ redis.php
@@ -7,14 +7,14 @@
  *
  * Usage:
  * $redis = new redis_cli ( '127.0.0.1', 6379 );
- * $redis -> cmd ( 'SET', 'foo', 'bar' ) -> set ();
- * $foo = $redis -> cmd ( 'GET', 'foo' ) -> get ();
+ * $redis->cmd ( 'SET', 'foo', 'bar' )->set ();
+ * $foo = $redis->cmd ( 'GET', 'foo' )->get ();
  *
- * $redis -> cmd ( 'HSET', 'hash', 'foo', 'bar' ) -> cmd ( 'HSET', 'hash', 'abc', 'def' ) -> set ();
- * $vals = $redis -> cmd ( 'HVALS', 'hash' ) -> get ();
+ * $redis->cmd ( 'HSET', 'hash', 'foo', 'bar' )->cmd ( 'HSET', 'hash', 'abc', 'def' )->set ();
+ * $vals = $redis->cmd ( 'HVALS', 'hash' )->get ();
  *
- * $redis -> cmd ( 'KEYS', 'online*' );
- * $total_online = $redis -> get_len ();
+ * $redis->cmd ( 'KEYS', 'online*' );
+ * $total_online = $redis->get_len ();
  *
  * Based on http://redis.io/topics/protocol
  */
@@ -32,7 +32,7 @@ class redis_cli
     private $port;
     private $silent_fail;
 
-    private $commands = array ();
+    private $commands = array();
 
     //Timeout for stream, 30 seconds
     private $timeout = 30;
@@ -49,160 +49,138 @@ class redis_cli
     //Error handling function, use set_error_function method ()
     private $error_function = null;
 
-    public function __construct ( $host = false, $port = false, $silent_fail = false, $timeout = 60 )
+    public function __construct($host = false, $port = false, $silent_fail = false, $timeout = 60)
     {
-        if ( $host && $port )
-        {
-            $this -> connect ( $host, $port, $silent_fail, $timeout );
+        if ($host && $port) {
+            $this->connect($host, $port, $silent_fail, $timeout);
         }
     }
 
     //Main method to establish connection
-    public function connect ( $host = '127.0.0.1', $port = 6379, $silent_fail = false, $timeout = 60 )
+    public function connect($host = '127.0.0.1', $port = 6379, $silent_fail = false, $timeout = 60)
     {
-        $this -> host = $host;
-        $this -> port = $port;
-        $this -> silent_fail = $silent_fail;
-        $this -> timeout = $timeout;
+        $this->host = $host;
+        $this->port = $port;
+        $this->silent_fail = $silent_fail;
+        $this->timeout = $timeout;
 
-        if ( $silent_fail )
-        {
-            $this -> handle = @fsockopen ( $host, $port, $errno, $errstr, $this -> connect_timeout );
+        if ($silent_fail) {
+            $this->handle = @fsockopen($host, $port, $errno, $errstr, $this->connect_timeout);
 
-            if ( !$this ->  handle )
-            {
-                $this -> handle = false;
+            if (!$this->handle) {
+                $this->handle = false;
             }
-        }
-        else
-        {
-            $this -> handle = fsockopen ( $host, $port, $errno, $errstr, $this -> connect_timeout );
+        } else {
+            $this->handle = fsockopen($host, $port, $errno, $errstr, $this->connect_timeout);
         }
 
-        if ( is_resource ( $this -> handle ) )
-        {
-            stream_set_timeout ( $this -> handle, $this -> timeout );
+        if (is_resource($this->handle)) {
+            stream_set_timeout($this->handle, $this->timeout);
         }
     }
 
-    public function reconnect (  )
+    public function reconnect()
     {
-        $this -> __destruct ();
-        $this -> connect ( $this -> host, $this -> port, $this -> silent_fail );
+        $this->__destruct();
+        $this->connect($this->host, $this->port, $this->silent_fail);
     }
 
-    public function __destruct ()
+    public function __destruct()
     {
-        if ( is_resource ( $this -> handle ) )
-        {
-            fclose ( $this -> handle );
+        if (is_resource($this->handle)) {
+            fclose($this->handle);
         }
     }
 
     //Returns all commands array
-    public function commands ()
+    public function commands()
     {
-        return $this -> commands;
+        return $this->commands;
     }
 
     //Used to push single command to queue
-    public function cmd ()
+    public function cmd()
     {
-        if ( !$this -> handle )
-        {
+        if (!$this->handle) {
             return $this;
         }
 
-        $args = func_get_args ();
-        $rlen = count ( $args );
+        $args = func_get_args();
+        $rlen = count($args);
 
         $output = '*'. $rlen . self::NL;
 
-        foreach ( $args as $arg )
-        {
-            $output .= '$'. strlen ( $arg ) . self::NL . $arg . self::NL;
+        foreach ($args as $arg) {
+            $output .= '$'. strlen($arg) . self::NL . $arg . self::NL;
         }
 
-        $this -> commands [] = $output;
+        $this->commands[] = $output;
 
         return $this;
     }
 
     //Used to push many commands at once, almost always for setting something
-    public function set ()
+    public function set()
     {
-        if ( !$this -> handle )
-        {
+        if (!$this->handle) {
             return false;
         }
 
         //Total size of commands
-        $size = $this -> exec ();
-        $response = array ();
+        $size = $this->exec();
+        $response = array();
 
-        for ( $i=0; $i<$size; $i++ )
-        {
-            $response [] = $this -> get_response ();
+        for ($i=0; $i<$size; $i++) {
+            $response[] = $this->get_response();
         }
 
-        if ( $this -> force_reconnect )
-        {
-            $this -> reconnect ();
+        if ($this->force_reconnect) {
+            $this->reconnect();
         }
 
         return $response;
     }
 
     //Used to get command response
-    public function get ( $line = false )
+    public function get($line = false)
     {
-        if ( !$this -> handle )
-        {
+        if (!$this->handle) {
             return false;
         }
 
         $return = false;
 
-        if ( $this -> exec () )
-        {
-            $return = $this -> get_response ();
+        if ($this->exec()) {
+            $return = $this->get_response();
 
-            if ( $this -> force_reconnect )
-            {
-                $this -> reconnect ();
+            if ($this->force_reconnect) {
+                $this->reconnect();
             }
-
         }
 
         return $return;
     }
 
     //Used to get length of the returned array. Most useful with `Keys` command
-    public function get_len ()
+    public function get_len()
     {
-        if ( !$this -> handle )
-        {
+        if (!$this->handle) {
             return false;
         }
 
         $return = null;
 
-        if ( $this -> exec () )
-        {
-            $char = fgetc ( $this -> handle );
+        if ($this->exec()) {
+            $char = fgetc($this->handle);
 
-            if ( $char == self::BULK )
-            {
-                $return = sizeof ( $this -> bulk_response () );
-            }
-            elseif ( $char == self::MULTIBULK )
-            {
-                $return = sizeof ( $this -> multibulk_response () );
+            if ($char == self::BULK) {
+                $return = sizeof($this->bulk_response());
+            } elseif ($char == self::MULTIBULK) {
+                $return = sizeof($this->multibulk_response());
             }
 
-            if ( $this -> force_reconnect )
-            {
-                $this -> reconnect ();
+            if ($this->force_reconnect) {
+                $this->reconnect();
             }
         }
 
@@ -210,35 +188,34 @@ class redis_cli
     }
 
     //Forces to reconnect after every get() or set(). Use this with extreme caution
-    public function set_force_reconnect ( $flag )
+    public function set_force_reconnect($flag)
     {
-        $this -> force_reconnect = $flag;
+        $this->force_reconnect = $flag;
         return $this;
     }
 
     //Used to parse single command single response
-    private function get_response ()
+    private function get_response()
     {
         $return = false;
 
-        $char = fgetc ( $this -> handle );
+        $char = fgetc($this->handle);
 
-        switch ( $char )
-        {
+        switch ($char) {
             case self::INLINE:
-                $return = $this -> inline_response ();
+                $return = $this->inline_response();
                 break;
             case self::INTEGER:
-                $return = $this -> integer_response ();
+                $return = $this->integer_response();
                 break;
             case self::BULK:
-                $return = $this -> bulk_response ();
+                $return = $this->bulk_response();
                 break;
             case self::MULTIBULK:
-                $return = $this -> multibulk_response ();
+                $return = $this->multibulk_response();
                 break;
             case self::ERROR:
-                $return = $this -> error_response ();
+                $return = $this->error_response();
                 break;
         }
 
@@ -246,72 +223,61 @@ class redis_cli
     }
 
     //For inline responses only
-    private function inline_response ()
+    private function inline_response()
     {
-        return trim ( fgets ( $this -> handle ) );
+        return trim(fgets($this->handle));
     }
 
     //For integer responses only
-    private function integer_response ()
+    private function integer_response()
     {
-        return ( int ) trim ( fgets ( $this -> handle ) );
+        return ( int ) trim(fgets($this->handle));
     }
 
     //For error responses only
-    private function error_response ()
+    private function error_response()
     {
-        $error = fgets ( $this -> handle );
+        $error = fgets($this->handle);
 
-        if ( $this -> error_function )
-        {
-            call_user_func ( $this -> error_function, $error .'('. $this -> last_used_command .')' );
+        if ($this->error_function) {
+            call_user_func($this->error_function, $error .'('. $this->last_used_command .')');
         }
 
         return false;
     }
 
     //For bulk responses only
-    private function bulk_response ()
+    private function bulk_response()
     {
-        $return = trim ( fgets ( $this -> handle ) );
+        $return = trim(fgets($this->handle));
 
-        if ( $return === '-1' )
-        {
+        if ($return === '-1') {
             $return = null;
-        }
-        else
-        {
-            $return = $this -> read_bulk_response ( $return );
+        } else {
+            $return = $this->read_bulk_response($return);
         }
 
         return $return;
     }
 
     //For multibulk responses only
-    private function multibulk_response ()
+    private function multibulk_response()
     {
-        $size = trim ( fgets ( $this -> handle ) );
+        $size = trim(fgets($this->handle));
         $return = false;
 
-        if ( $size === '-1' )
-        {
+        if ($size === '-1') {
             $return = null;
-        }
-        else
-        {
-            $return = array ();
+        } else {
+            $return = array();
 
-            for ( $i = 0; $i < $size; $i++ )
-            {
-                $tmp = trim ( fgets ( $this -> handle ) );
+            for ($i = 0; $i < $size; $i++) {
+                $tmp = trim(fgets($this->handle));
 
-                if ( $tmp === '-1' )
-                {
-                    $return [] = null;
-                }
-                else
-                {
-                    $return [] = $this -> read_bulk_response ( $tmp );
+                if ($tmp === '-1') {
+                    $return[] = null;
+                } else {
+                    $return[] = $this->read_bulk_response($tmp);
                 }
             }
         }
@@ -320,59 +286,56 @@ class redis_cli
     }
 
     //Sends command to the redis
-    private function exec ()
+    private function exec()
     {
-        $size = sizeof ( $this -> commands );
+        $size = sizeof($this->commands);
 
-        if ( $size < 1 )
-        {
+        if ($size < 1) {
             return null;
         }
 
-        if ( $this -> error_function )
-        {
-            $this -> last_used_command = str_replace ( self::NL, '\\r\\n', implode ( ';', $this -> commands ) );
+        if ($this->error_function) {
+            $this->last_used_command = str_replace(self::NL, '\\r\\n', implode(';', $this->commands));
         }
 
-        $command = implode ( self::NL, $this -> commands ) . self::NL;
-        fwrite ( $this -> handle, $command );
+        $command = implode(self::NL, $this->commands) . self::NL;
+        fwrite($this->handle, $command);
 
-        $this -> commands = array ();
+        $this->commands = array();
         return $size;
     }
 
     //Bulk response reader
-    private function read_bulk_response ( $tmp )
+    private function read_bulk_response($tmp)
     {
         $response = null;
 
         $read = 0;
-        $size = ( ( strlen ( $tmp ) > 1 && substr ( $tmp, 0, 1 ) === self::BULK ) ? substr ( $tmp, 1 ) : $tmp );
+        $size = ((strlen($tmp) > 1 && substr($tmp, 0, 1) === self::BULK) ? substr($tmp, 1) : $tmp);
 
-        while ( $read < $size )
-        {
+        while ($read < $size) {
             $diff = $size - $read;
 
             $block_size = $diff > 8192 ? 8192 : $diff;
 
-            $chunk = fread ( $this -> handle, $block_size );
-            
-            if($chunk){
+            $chunk = fread($this->handle, $block_size);
+
+            if ($chunk) {
                 $chunkLen = strlen($chunk);
                 $read += $chunkLen;
                 $response .= $chunk;
             } else {
-                fseek( $this -> handle, $read );
+                fseek($this->handle, $read);
             }
         }
 
-        fgets ( $this -> handle );
+        fgets($this->handle);
 
         return $response;
     }
 
-    public function set_error_function ( $func )
+    public function set_error_function($func)
     {
-        $this -> error_function = $func;
+        $this->error_function = $func;
     }
 }
--- tests.php
+++ tests.php
@@ -1,67 +1,67 @@
 <?php
 
-require dirname ( __FILE__ ) .'/redis.php';
+require dirname(__FILE__) .'/redis.php';
 
-define ( 'NL', ( PHP_SAPI == 'cli' ? "\n" : '<br />' ) );
+define('NL', (PHP_SAPI == 'cli' ? "\n" : '<br />'));
 
-$redis = new redis_cli ( '127.0.0.1', 6379 );
+$redis = new redis_cli('127.0.0.1', 6379);
 //$redis -> cmd ( 'SELECT', 9 );
 
 /////////////////////////////////////////////////////////////
 
-$redis -> cmd ( 'SET', 'foo', 'bar' ) -> set ();
-$foo = $redis -> cmd ( 'GET', 'foo' ) -> get ();
-var_dump ( $foo );
+$redis -> cmd('SET', 'foo', 'bar') -> set();
+$foo = $redis -> cmd('GET', 'foo') -> get();
+var_dump($foo);
 
-echo NL . str_repeat ( '-', 20 ) . NL . NL;
-
-/////////////////////////////////////////////////////////////
-
-$redis -> cmd ( 'HSET', 'hash', 'foo', 'bar' ) -> cmd ( 'HSET', 'hash', 'abc', 'def' ) -> cmd ( 'HSET', 'hash', '123', '456' ) -> set ();
-
-$vals = $redis -> cmd ( 'HGETALL', 'hash' ) -> get ();
-var_dump ( $vals );
-
-$total = $redis -> cmd ( 'HVALS', 'hash' ) -> get_len ();
-var_dump ( $total );
-
-echo NL . str_repeat ( '-', 20 ) . NL . NL;
+echo NL . str_repeat('-', 20) . NL . NL;
 
 /////////////////////////////////////////////////////////////
 
-$redis -> cmd ( 'INCR', 'online_foo' ) -> cmd ( 'INCR', 'online_bar' ) -> cmd ( 'INCRBY', 'online_foo', 3 ) -> set ();
+$redis -> cmd('HSET', 'hash', 'foo', 'bar') -> cmd('HSET', 'hash', 'abc', 'def') -> cmd('HSET', 'hash', '123', '456') -> set();
 
-$total_online = $redis -> cmd ( 'KEYS', 'online*' ) -> get_len ();
-var_dump ( $total_online );
+$vals = $redis -> cmd('HGETALL', 'hash') -> get();
+var_dump($vals);
 
-$foo_online = $redis -> cmd ( 'GET', 'online_foo' ) -> get ();
-var_dump ( $foo_online );
+$total = $redis -> cmd('HVALS', 'hash') -> get_len();
+var_dump($total);
 
-echo NL . str_repeat ( '-', 20 ) . NL . NL;
+echo NL . str_repeat('-', 20) . NL . NL;
 
 /////////////////////////////////////////////////////////////
 
-$hash = 'GO-'. date ( 'dHi' );
-$redis -> cmd ( 'SADD', $hash, 'Some data' )
-       -> cmd ( 'SADD', $hash, 'More data' )
-       -> cmd ( 'SADD', $hash, 'Even more data' )
-       -> cmd ( 'EXPIRE', $hash, 900 )
-       -> set ();
+$redis -> cmd('INCR', 'online_foo') -> cmd('INCR', 'online_bar') -> cmd('INCRBY', 'online_foo', 3) -> set();
 
-$expiration = $redis -> cmd ( 'TTL', $hash ) -> get ();
-var_dump ( $expiration );
+$total_online = $redis -> cmd('KEYS', 'online*') -> get_len();
+var_dump($total_online);
 
-$total = $redis -> cmd ( 'SCARD', $hash ) -> get ();
-var_dump ( $total );
+$foo_online = $redis -> cmd('GET', 'online_foo') -> get();
+var_dump($foo_online);
 
-$list = $redis -> cmd ( 'SMEMBERS', $hash ) -> get ();
-var_dump ( $list );
+echo NL . str_repeat('-', 20) . NL . NL;
 
-$redis -> cmd ( 'DEL', $hash ) -> set ();
-$list = $redis -> cmd ( 'SMEMBERS', $hash ) -> get ();
-var_dump ( $list );
+/////////////////////////////////////////////////////////////
 
-echo NL . str_repeat ( '-', 20 ) . NL . NL;
+$hash = 'GO-'. date('dHi');
+$redis -> cmd('SADD', $hash, 'Some data')
+       -> cmd('SADD', $hash, 'More data')
+       -> cmd('SADD', $hash, 'Even more data')
+       -> cmd('EXPIRE', $hash, 900)
+       -> set();
+
+$expiration = $redis -> cmd('TTL', $hash) -> get();
+var_dump($expiration);
+
+$total = $redis -> cmd('SCARD', $hash) -> get();
+var_dump($total);
+
+$list = $redis -> cmd('SMEMBERS', $hash) -> get();
+var_dump($list);
+
+$redis -> cmd('DEL', $hash) -> set();
+$list = $redis -> cmd('SMEMBERS', $hash) -> get();
+var_dump($list);
+
+echo NL . str_repeat('-', 20) . NL . NL;
 
 /////////////////////////////////////////////////////////////
 
